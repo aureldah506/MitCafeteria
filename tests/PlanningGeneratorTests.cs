cd068b8ed0f292c4559fa8f83edd10642bd0fcb3
aurel <aurel@pcaurel.com>
2026-03-30 16:36:50 +0200

    test: itération 4 - rotation des partenaires(passage au rouge)

--- tests/PlanningGeneratorTests.cs
+++ tests/PlanningGeneratorTests.cs
@@ -1,6 +1,5 @@
 ﻿namespace src;
 
-using src; 
 public class PlanningGeneratorTests
 {
     [Fact]
@@ -17,5 +16,25 @@ public class PlanningGeneratorTests
         // Assert
         Assert.Single(result); 
         Assert.Equal(3, result[0].Assignments.Count); 
+    }
+    
+    [Fact]
+    public void should_change_partners_between_shifts_when_multiple_shifts_are_requested()
+    {
+        // Arrange
+        string[] staff = { "Marcus", "Lateefa", "Donald", "Rashad", "Quincy", "Mia" };
+        string[] sections = { "Lobby", "Dining Room", "Kitchen" };
+        int[] shifts = { 0, 1 }; 
+        
+        // Act
+        var schedule = MitCafeteriaGenerator.Generate(staff, sections, shifts);
+
+        var partnerShift0 = schedule[0].Assignments.First(a => a.Employee1 == "Marcus").Employee2;
+        var partnerShift1 = schedule[1].Assignments.First(a => a.Employee1 == "Marcus" || a.Employee2 == "Marcus");
+    
+        string currentPartnerShift1 = (partnerShift1.Employee1 == "Marcus") ? partnerShift1.Employee2 : partnerShift1.Employee1;
+
+        // Assert
+        Assert.NotEqual(partnerShift0, currentPartnerShift1); 
     }
 }
